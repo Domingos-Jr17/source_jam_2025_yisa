683a9b4417159409c3c4ecf71b9e93fe8055c9dd
shelton111 <smatsinhe223@gmail.com>
2025-11-13 19:43:39 +0200

    Remove unused assets and configuration files
    - Deleted PostCSS configuration file as it is no longer needed.
    - Removed SVG and PNG assets including icons and logos from the public directory.
    - Eliminated global CSS file that contained Tailwind CSS imports and custom styles.
    - Removed Tailwind CSS configuration file to clean up the project.
    - Deleted TypeScript configuration file as it is not required anymore.

--- yisa/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import TelaAutenticacao from "@/components/telas/tela-autenticacao"
-
-export default function LoginPage() {
-  return <TelaAutenticacao />
-}
--- yisa/app/solicitar/page.tsx
+++
@@ -1,5 +0,0 @@
-import TelaSolicitacoes from "@/components/telas/tela-solicitacoes"
-
-export default function LoginPage() {
-  return <TelaSolicitacoes />
-}
